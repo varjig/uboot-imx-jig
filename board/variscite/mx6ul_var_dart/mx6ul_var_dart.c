e4a7211d972dd64bea4c7b752d2ee21a44afbbc1
Eran Matityahu <eran.m@variscite.com>
2018-10-31 20:41:46 +0200

    mx6ul_var_dart: In case of an i.MX6ULZ SOC, do not setup Ethernet
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- board/variscite/mx6ul_var_dart/mx6ul_var_dart.c
+++ board/variscite/mx6ul_var_dart/mx6ul_var_dart.c
@@ -628,55 +628,62 @@ static void setup_iomux_fec(int fec_id)
 
 int board_eth_init(bd_t *bis)
 {
-	int ret;
-	setup_iomux_fec(CONFIG_FEC_ENET_DEV);
+	int ret = 0;
+	if (!is_cpu_type(MXC_CPU_MX6ULZ)) {
+		setup_iomux_fec(CONFIG_FEC_ENET_DEV);
 
-	ret = fecmxc_initialize_multi(bis, CONFIG_FEC_ENET_DEV,
-				       CONFIG_FEC_MXC_PHYADDR, IMX_FEC_BASE);
+		ret = fecmxc_initialize_multi(bis, CONFIG_FEC_ENET_DEV,
+				CONFIG_FEC_MXC_PHYADDR, IMX_FEC_BASE);
+	}
 
 #if defined(CONFIG_CI_UDC) && defined(CONFIG_USB_ETHER)
 	/* USB Ethernet Gadget */
 	usb_eth_initialize(bis);
 #endif
+
 	return ret;
 }
 
 static int setup_fec(int fec_id)
 {
-	struct iomuxc *const iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
-	int ret;
+	if (!is_cpu_type(MXC_CPU_MX6ULZ)) {
+		struct iomuxc *const iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
+		int ret;
 
-	if (fec_id == 0) {
-		/*
-		 * Use 50M anatop loopback REF_CLK1 for ENET1,
-		 * clear gpr1[13], set gpr1[17].
-		 */
-		clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
-				IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
-	} else {
-		/*
-		 * Use 50M anatop loopback REF_CLK2 for ENET2,
-		 * clear gpr1[14], set gpr1[18].
-		 */
-		clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC2_MASK,
-				IOMUX_GPR1_FEC2_CLOCK_MUX1_SEL_MASK);
+		if (fec_id == 0) {
+			/*
+			 * Use 50M anatop loopback REF_CLK1 for ENET1,
+			 * clear gpr1[13], set gpr1[17].
+			 */
+			clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
+					IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
+		} else {
+			/*
+			 * Use 50M anatop loopback REF_CLK2 for ENET2,
+			 * clear gpr1[14], set gpr1[18].
+			 */
+			clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC2_MASK,
+					IOMUX_GPR1_FEC2_CLOCK_MUX1_SEL_MASK);
+		}
+
+		ret = enable_fec_anatop_clock(fec_id, ENET_50MHZ);
+		if (ret)
+			return ret;
+
+		enable_enet_clk(1);
 	}
-
-	ret = enable_fec_anatop_clock(fec_id, ENET_50MHZ);
-	if (ret)
-		return ret;
-
-	enable_enet_clk(1);
 
 	return 0;
 }
 
 int board_phy_config(struct phy_device *phydev)
 {
-	phy_write(phydev, MDIO_DEVAD_NONE, 0x1f, 0x8190);
+	if (!is_cpu_type(MXC_CPU_MX6ULZ)) {
+		phy_write(phydev, MDIO_DEVAD_NONE, 0x1f, 0x8190);
 
-	if (phydev->drv->config)
-		phydev->drv->config(phydev);
+		if (phydev->drv->config)
+			phydev->drv->config(phydev);
+	}
 
 	return 0;
 }
