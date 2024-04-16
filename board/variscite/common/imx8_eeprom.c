varigit/uboot-imx

variscite: imx8_eeprom.h: add support for major and minor som revision
The lower 5 bits of ep->somrev are the minor version.
The upper 3 bits of ep->somrev are the major version.

Signed-off-by: Nate Drude <nate.d@variscite.com>

board/variscite/common/imx8_eeprom.c
@@ -235,7 +235,7 @@ void var_eeprom_print_prod_info(struct var_eeprom *ep)
 
 	debug("EEPROM version: 0x%x\n", ep->version);
 	debug("SOM features: 0x%x\n", ep->features);
-	debug("SOM revision: 0x%x\n", ep->somrev);
+	printf("SOM revision: %ld.%ld\n", SOMREV_MAJOR(ep->somrev), SOMREV_MINOR(ep->somrev));
 
 	if (ep->version == 1)
 		debug("DRAM size: %d GiB\n\n", ep->dramsize);

varjig/uboot-imx-jig

board/variscite/common/imx8_eeprom.c
@@ -208,9 +208,10 @@ void var_eeprom_print_prod_info(struct var_eeprom *e)
 	printf("Serial Number: %02x:%02x:%02x:%02x:%02x:%02x\n",
 		e->mac[0], e->mac[1], e->mac[2], e->mac[3], e->mac[4], e->mac[5]);
 
+
 	debug("EEPROM version: 0x%x\n", e->version);
 	debug("SOM features: 0x%x\n", e->features);
-	debug("SOM revision: 0x%x\n", e->somrev);
+	printf("SOM revision: %ld.%ld\n", SOMREV_MAJOR(e->somrev), SOMREV_MINOR(e->somrev));
 
 	if (e->version == 1)
 		debug("DRAM size: %d GiB\n\n", e->dramsize);