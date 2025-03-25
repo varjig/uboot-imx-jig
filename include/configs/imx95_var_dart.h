f877330e8e23ee7b9f764af97a39bff6318ba27a
Yair Ben Avraham <yair.b@variscite.com>
2025-03-25 17:15:16 +0200

    Remove SD Card Detect for iMX95 Jig Adapter
    The iMX95 jig adapter requires the card detect line to be disabled for proper
    operation. This commit:
    - Removes the card detect GPIO configuration from the device tree
    - Adds broken-cd property to indicate card detection is disabled
    - Simplifies the FDT file selection logic since we no longer need to handle
      different carrier board revisions
    
    Signed-off-by: Yair Ben Avraham <yair.b@variscite.com>

--- include/configs/imx95_var_dart.h
+++ include/configs/imx95_var_dart.h
@@ -84,19 +84,7 @@
 		"unzip ${img_addr} ${loadaddr}\0" \
 	"findfdt=" \
 		"if test $fdt_file = undefined; then " \
-			"if setexpr substring sub sonata sonata ${carrier_rev}; then " \
-				"if test $som_rev = 1.0; then " \
-					"setenv fdt_file imx95-var-dart-1.0-sonata.dtb; " \
-				"else " \
-					"setenv fdt_file imx95-var-dart-sonata.dtb; " \
-				"fi; " \
-			"else " \
-				"if test $som_rev = 1.0; then " \
-					"setenv fdt_file imx95-var-dart-1.0-dt8mcustomboard.dtb; " \
-				"else " \
-					"setenv fdt_file imx95-var-dart-dt8mcustomboard.dtb; " \
-				"fi; " \
-			"fi; " \
+			"setenv fdt_file imx95-var-dart-jig.dtb; " \
 		"fi; \0" \
 	"loadfdt=run findfdt; " \
 		"echo fdt_file=${fdt_file}; " \
